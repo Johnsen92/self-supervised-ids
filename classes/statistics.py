fbc96075e32c508fea13175e51770a3c33d14a30
Jonas Ferdigg <e1226597@student.tuwien.ac.at>
2021-05-31 23:50:28 +0200

    added PDPlot class

--- classes/statistics.py
+++ classes/statistics.py
@@ -8,6 +8,9 @@ from datetime import timedelta
 import json
 import os
 import errno
+from collections import Counter
+from matplotlib.lines import Line2D
+from matplotlib.patches import Rectangle
 
 def formatTime(time_s):
     time_h = time_s // 3600
@@ -199,7 +202,6 @@ class ClassStats():
                     raise
             
 class Stats():
-
     index = 0
 
     def __init__(self, stats_dir='./', n_samples=None, train_percent=None, pretrain_percent=None, proxy_task=None, val_percent=None, n_epochs=None, n_epochs_pretraining=None, model_parameters=None, batch_size=None, learning_rate=None, losses=None, class_stats=None, n_false_positive=None, n_false_negative=None, title=None, random_seed=None, subset=False):
@@ -339,3 +341,72 @@ class Stats():
             return 0.0
         else:
             return max([acc for _, acc in self.accuracies]) * 100.0
+
+class DPPlot():
+    def __init__(self, categories_mapping, mapping, results_by_attack_number, feature_names, feature_values_by_attack_number, file_name):
+        reverse_mapping = {v: k for k, v in mapping.items()}
+        colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+        DIR_NAME = "plots/plot_pdp"
+
+        display_names = {'srcPort': 'Source port', 'dstPort': 'Destination port'}
+
+        for attack_type, (all_features, all_features_values) in enumerate(zip(results_by_attack_number, feature_values_by_attack_number)):
+
+            print("attack_type", attack_type)
+            fig, ax1 = plt.subplots(figsize=(5,2.4))
+
+            ax2 = ax1.twinx()
+
+            ax2.set_ylabel('Prediction')
+            ax1.set_ylabel("Flow number")
+
+            ax1.yaxis.tick_right()
+            ax1.yaxis.set_label_position("right")
+            ax2.yaxis.tick_left()
+            ax2.yaxis.set_label_position("left")
+
+            if all_features is None:
+                continue
+            # print("all_features.shape", all_features.shape)
+            all_legends = []
+            all_labels = []
+            for feature_name, feature_index in zip(feature_names, range(all_features.shape[0])):
+
+                as_ints = list(all_features_values[feature_index].astype(np.int32))
+
+                # print("all_features_values[feature_index]", all_features_values[feature_index])
+                # ret1 = ax1.hist(all_features_values[feature_index], bins=range(int(round(all_features_values[feature_index].max())+1)), width=1, color=colors[feature_index], alpha=0.2, label="{} occurrence".format(feature_name))
+
+                counted = Counter(as_ints)
+                keys = counted.keys()
+                values = counted.values()
+
+                # print("keys", keys, "values", values)
+                ret1 = ax1.bar(keys, values, width=1000, color=colors[feature_index], alpha=0.2, label="{} occurrence".format(feature_name))
+
+                ret2 = ax2.plot(all_features[feature_index,0,:], all_features[feature_index,1,:], color=colors[feature_index], label="{} confidence".format(feature_name))
+                # all_legends.append(feature_name)
+                # print("legend", legend)
+                all_legends.append(Rectangle((0,0), 1, 1, color=colors[feature_index]))
+                all_labels.append(display_names[feature_name])
+                # all_legends += ret2
+
+            # plt.title(reverse_mapping[attack_type])
+            # print("all_legends", all_legends)
+            ax1.set_yscale('log')
+            ax1.set_ylim((ax1.get_ylim()[0], 1000))
+            ax2.set_ylim((ax2.get_ylim()[0], 0.7))
+            # all_labels = [item.get_label() for item in all_legends]
+            ax2.legend(all_legends[::-1], all_labels[::-1], loc='upper left', bbox_to_anchor=(0.06,1))
+            ax1.set_xlabel('Port number')
+            ax2.set_ylabel('Partial dependence')
+            ax2.set_ylabel_legend(Line2D([0],[0], color='gray'))
+            ax1.set_ylabel_legend(Rectangle((0,0), 1,1, fc='gray', alpha=0.2), handlelength=0.7)
+            plt.tight_layout()
+            #plt.savefig('%s.pdf' % os.path.splitext(fn)[0])
+            # plt.show()
+
+            os.makedirs(DIR_NAME, exist_ok=True)
+            plt.savefig(DIR_NAME+'/{}_{}_{}.pdf'.format(file_name.split("/")[-1], attack_type, reverse_mapping[attack_type].replace("/", "-").replace(":", "-")), bbox_inches = 'tight', pad_inches = 0)
+            plt.clf()
